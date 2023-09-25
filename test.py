aeb170329f937ead8ef2a465a512bde93859ff51
Zak Richards <145971994+zakr0112@users.noreply.github.com>
2023-09-25 11:15:52 +0100

    Update test.py
    Appended the file based on local copy, this was uploaded via github website.

--- test.py
+++ test.py
@@ -1,2 +1,4 @@
 if __name__ == '__main__':
-    print("test")
+    print("This file is used as a test for github file uploading via jetbrains IDE")
+    print("testgit push --herl"
+          "dsrg")
